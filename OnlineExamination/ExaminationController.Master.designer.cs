169cc6387d093e0f550dbddc7b71bd574ec7fdc0
Rameshwar <Rameshwar@DESKTOP-FINEL>
2018-08-12 00:16:36 +0530

    changes

--- OnlineExamination/ExaminationController.Master.designer.cs
+++ OnlineExamination/ExaminationController.Master.designer.cs
@@ -112,6 +112,15 @@ namespace OnlineExamination {
         protected global::System.Web.UI.WebControls.Button lbtnChangePwd;
         
         /// <summary>
+        /// lbtn_StudeReg control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button lbtn_StudeReg;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
